b8c9e2db3d9ed2a70b606677f0315bd77605b691
kristina <savkris@icloud.com>
2016-02-22 21:47:11 +1100

    - created project - added models (customer, bet) - added CSVfiles into solution

---
+++ WHtest/src/Models/Bet.java
@@ -0,0 +1,10 @@
+package Models;
+
+public class Bet {
+
+	public Boolean IsSettled;		
+	public long EventID;
+	public long ParticipantID; 
+	public long StakeAmount;
+	public long WinAmount; 	// if no win == 0	
+}
---
+++ WHtest/src/Models/Customer.java
@@ -0,0 +1,11 @@
+package Models;
+
+import java.util.List;
+
+public class Customer 
+{
+	public long CustomerID;
+	public List<Bet> SettledBetsList;
+	public List<Bet> UnSettledBetsList;
+	
+}
